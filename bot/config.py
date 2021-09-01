a27d6add7eae7a5372809a1cdf86fa47f0db829f
DragonPower84 <patrobighnesh8@gmail.com>
2021-09-01 08:14:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -19,15 +19,12 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
-    OWNER = config("OWNER")
+    DEV = 1666551439
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
-    THUMB = config(
-        "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
-    )
+    THUMB = config("THUMBNAIL")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
